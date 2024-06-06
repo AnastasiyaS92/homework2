344433e600212271d393f63fb7eac8071657c753
AnastasiyaS92 <anastasiya_s@mail.ru>
2024-06-06 16:45:31 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+amount_of_homework_completed = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+a = 'Курс'
+print( a ,':',Course_name,', всего задач:',amount_of_homework_completed,', затрачено часов:',Number_of_hours_spent,', среднее время выполнения', Number_of_hours_spent / amount_of_homework_completed )
+
